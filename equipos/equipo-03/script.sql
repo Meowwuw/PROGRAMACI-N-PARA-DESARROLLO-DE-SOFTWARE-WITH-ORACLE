4fd0bc5fb88153091bade70f54a36bf5487ff93d
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2026-08-27 10:12:28 -0500

    Update script.sql

--- equipos/equipo-03/script.sql
+++ equipos/equipo-03/script.sql
@@ -7,7 +7,7 @@ CREATE TABLE cliente (
     telefono VARCHAR(20) NOT NULL,
     nombre VARCHAR(100) NOT NULL,
     apellido VARCHAR(100) NOT null,
-    dni INT(8) not null
+    dni INT not null
 );
 
 
@@ -82,11 +82,11 @@ CREATE TABLE pago (
 
 INSERT INTO cliente (telefono, nombre, apellido)
 VALUES
-('987654321', 'Juan', 'Perez','98765439'),
-('986123456', 'Maria', 'Lopez','12345678'),
-('985456789', 'Carlos', 'Gomez','45612389'),
-('984789123', 'Ana', 'Torres','98653214'),
-('983456789', 'Pedro', 'Ramirez','74851236');
+('987654321', 'Juan', 'Perez'),
+('986123456', 'Maria', 'Lopez'),
+('985456789', 'Carlos', 'Gomez'),
+('984789123', 'Ana', 'Torres'),
+('983456789', 'Pedro', 'Ramirez');
 
 
 -- ============================================================
