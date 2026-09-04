05d29558953d8e0fe743e927ca4103da53953247
nicolasaguilar05 <dieguito_ap5@hotmail.com>
2026-09-04 11:02:40 -0500

    Add files via upload

---
+++ equipos/equipo-04/Backend/Perfumeria/src/main/java/com/example/backend/model/Producto.java
@@ -0,0 +1,21 @@
+package com.example.backend.model;
+
+public class Producto {
+    private Long id;
+    private String nombre;
+    private Double precio;
+    public Producto (Long id, String nombre, Double precio){
+        this.id=id;
+        this.nombre=nombre;
+        this.precio=precio;
+    }
+    public Long getId(){
+        return id;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public Double getPrecio(){
+        return precio;
+    }
+}
