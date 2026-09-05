90c46834e01b660a97b85eb7d3ae0cb1539712d5
PatrickFreytas <135546208+PatrickFreytas@users.noreply.github.com>
2026-09-05 10:08:46 -0500

    chore: "clean imports"

--- equipos/equipo-03/backend/src/main/java/com/canchavoley/backend/controller/clienteController.java
+++ equipos/equipo-03/backend/src/main/java/com/canchavoley/backend/controller/clienteController.java
@@ -1,11 +1,8 @@
 package com.canchavoley.backend.controller;
 import com.canchavoley.backend.model.Cliente;
 
-import com.canchavoley.backend.model.Producto;
 import com.canchavoley.backend.service.ClienteService;
-import com.canchavoley.backend.service.ProductoService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
