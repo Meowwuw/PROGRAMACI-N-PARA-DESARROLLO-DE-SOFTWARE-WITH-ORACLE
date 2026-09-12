562b4833d773d2be798a5e33fe541ef0a6d0547f
TuNombre <tu_correo@gmail.com>
2026-09-12 10:58:13 -0500

    Agrego mis avances al proyecto

--- equipos/equipo-05/Backend/VoleyPlay/src/main/java/com/VoleyPlay/backend/controller/HorarioController.java
+++ equipos/equipo-05/Backend/VoleyPlay/src/main/java/com/VoleyPlay/backend/controller/HorarioController.java
@@ -1,35 +1,28 @@
 package com.VoleyPlay.backend.controller;
 
 import com.VoleyPlay.backend.model.Horario;
-import com.VoleyPlay.backend.services.HorarioService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.VoleyPlay.backend.Repository.HorarioRepository; // R mayúscula
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/horario")
+@CrossOrigin(origins = "*")
 public class HorarioController {
-    private final HorarioService horarioService;
 
-    public HorarioController(HorarioService horarioService) {
-        this.horarioService = horarioService;
-    }
+    @Autowired
+    private HorarioRepository horarioRepository;
 
     @GetMapping
-    public List<Horario> listar() {
-        return horarioService.listar();
+    public List<Horario> obtenerTodos() {
+        return horarioRepository.findAll();
     }
 
-    @GetMapping("/destacado")
-    public Horario destacado() {
-        return new Horario(1L, "18:00", "20:00", 80.00);
-    }
-
-    @GetMapping("{id}")
-    public Horario buscarPorId(@PathVariable Long id) {
-        return new Horario(id, "00:00", "00:00", 0.0);
+    @PostMapping
+    public Horario guardarHorario(@RequestBody Horario horario) {
+        return horarioRepository.save(horario);
     }
 }
