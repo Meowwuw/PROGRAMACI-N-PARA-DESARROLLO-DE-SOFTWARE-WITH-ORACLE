b7f8c3b3c8dd0e79e64242f0d9961897f1300179
Magenta Paredes <85425689+Meowwuw@users.noreply.github.com>
2026-09-18 10:00:38 -0500

    Delete equipos/equipo-05/Backend/VoleyP directory

--- equipos/equipo-05/Backend/VoleyP/src/main/java/com/canchavoley/backend/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.canchavoley.backend.repository;
-
-import com.canchavoley.backend.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository <Cliente, Long> {
-
-}
