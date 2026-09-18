4936a8e41354881b4c7e87e6cc7f3b8d3f1239b9
Magenta Paredes <85425689+Meowwuw@users.noreply.github.com>
2026-09-18 10:00:58 -0500

    Delete equipos/equipo-05/Backend/voley_playa directory

--- equipos/equipo-05/Backend/voley_playa/src/main/java/com/voley_playa/bakend/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.voley_playa.bakend.repository;
-
-import com.voley_playa.bakend.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
