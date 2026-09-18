7d5f1b3561760c01f528b062be85bbb6eece2fc0
Clara Luz <cheroriosc@gmail.com>
2026-09-18 09:41:38 -0500

    feat:agregue metodos

--- equipos/equipo-05/Backend/VoleyPlay/src/main/java/com/VoleyPlay/backend/controller/ReservaController.java
+++ equipos/equipo-05/Backend/VoleyPlay/src/main/java/com/VoleyPlay/backend/controller/ReservaController.java
@@ -2,16 +2,14 @@ package com.VoleyPlay.backend.controller;
 
 import com.VoleyPlay.backend.model.Reserva;
 import com.VoleyPlay.backend.services.ReservaService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/reserva")
+@RequestMapping("/api/reservas")
 public class ReservaController {
+
     private final ReservaService reservaService;
 
     public ReservaController(ReservaService reservaService) {
@@ -28,8 +26,10 @@ public class ReservaController {
         return new Reserva(1L, 1L, 1L, 1L, "2026-09-06", "Confirmada", 50.00);
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     public Reserva buscarPorId(@PathVariable Long id) {
         return new Reserva(id, 0L, 0L, 0L, "1900-01-01", "Pendiente", 0.0);
     }
+
+
 }
