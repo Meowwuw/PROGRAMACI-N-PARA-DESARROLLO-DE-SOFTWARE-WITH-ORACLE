1796e13fffbf291a0ec6419e047bea42c5412a66
Magenta Paredes <85425689+Meowwuw@users.noreply.github.com>
2026-09-24 10:24:42 -0500

    Delete equipos/equipo-05/Backend directory

--- equipos/equipo-05/Backend/VoleyPlay/src/main/java/com/VoleyPlay/backend/repository/PagoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.VoleyPlay.backend.repository;
-import com.VoleyPlay.backend.model.Pago;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PagoRepository
-        extends JpaRepository <Pago, Long> {
-}
